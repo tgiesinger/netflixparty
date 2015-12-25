e56b1d9badd6d4ba998edef8fbcef91a10720bda
stephan-boyer <stephan.boyer@airbnb.com>
2015-12-25 12:05:00 -1000

    basic web framework

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 var express = require('express');
 var app = express();
 
+// request logging
+var morgan = require('morgan');
+app.use(morgan('short'));
+
+// compress responses
+var compression = require('compression');
+app.use(compression());
+
+// parse request bodies
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// turn off unnecessary header
+app.disable('x-powered-by');
+
+// turn on strict routing
+app.enable('strict routing');
+
+// use the X-Forwarded-* headers
+app.enable('trust proxy');
+
+// template engine
+app.set('view engine', 'garnet');
+
+// favicon
+var favicon = require('serve-favicon');
+app.use(favicon(__dirname + '/static/favicon.ico'));
+
 app.get('/', function(req, res) {
-  res.send('Hello, World!');
+  res.render('index.garnet');
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
